2679da04debfdfc113bd92096e80ac76340b2b06
SowntharyaMahendran <30342261+SowntharyaMahendran@users.noreply.github.com>
2017-07-21 17:14:52 +0530

    Update and rename .gitignore to largest.c

---
+++ .gitignore/largest.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("enter the values",a,b);
+scanf("%d%d",&a,&b);
+if(a<b)
+{
+printf("b is greater");
+}
+elseif(b<c)
+{
+printf("c is greater");
+}
+else if(c<a)
+{
+printf("a is greater");
+}
+return 0;
+}
