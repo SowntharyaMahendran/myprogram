886df0fd71f418d0f29fc88578a2da96f9c3ce7f
SowntharyaMahendran <30342261+SowntharyaMahendran@users.noreply.github.com>
2017-07-23 13:55:23 +0530

    Create vowcon.c

---
+++ vowcon.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int n;
+char a,e,i,o,u,A,E,I,O,U;
+printf("enter a letter: ",n);
+scanf("%d",&n);
+if((n=a)||(n=e)||(n=i)||(n=o)||(n=u)||(n=A)||(n=E)||(n=I)||(n=O)||(n=U))
+{
+printf("vowel");
+else
+printf("consonant");
+}
+return 0;
+}
